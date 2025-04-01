6b474f3192586cc3cf684f840dfd5bf6f0668b62
Cainan Rodrigues <cainan.web@gmail.com>
2025-03-31 22:16:13 -0300

    initial api test

--- app.py
+++ app.py
@@ -12,3 +12,26 @@ users = [
 class UserList(Resource):
     def get(self):
         return jsonify(users)
+
+    def post(self):
+        new_user = request.get_json()
+        users.append(new_user)
+        return jsonify({"message": "New user added", "user": new_user})
+
+class User(Resource):
+    def get(self, user_id):
+        user = next((u for u in users if u["id"] == user_id), None)
+        if user:
+            return jsonify(user)
+        return jsonify({"message": "User not found"}), 404
+
+    def delete(self, user_id):
+        global users
+        user = [u for u in users if u["id"] != user_id]
+        return jsonify({"message": "User deleted"})
+
+api.add_resource(UserList, '/users')
+api.add_resource(User, '/users/<int:user_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
