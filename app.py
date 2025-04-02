b16cda0007e88e87968a74337884bab2135a535d
Cainan Rodrigues <cainan.web@gmail.com>
2025-04-01 22:18:30 -0300

    Creation of the accountBalance class

--- app.py
+++ app.py
@@ -4,21 +4,30 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-users = [
-    {"id": 1, "name": "Cainan"},
-    {"id": 2, "name": "Rob"}
-]
+accounts = []
 
-class UserList(Resource):
+class accountBalance(Resource):
     def get(self):
-        return jsonify(users)
+        try:
+            account_id = int(request.args.get('account_id'))
+        except Exception:
+            response = jsonify({"message": "Invalid or missing account_id"})
+            response.status_code = 400
+            return response
 
-    def post(self):
-        new_user = request.get_json()
-        users.append(new_user)
-        return jsonify({"message": "New user added", "user": new_user})
+        account = next((acc for acc in accounts if acc["id"] == account_id), None)
 
-class User(Resource):
+        if account:
+            response = jsonify(account["value"])
+            response.status_code = 200
+        else:
+            response = jsonify(0)
+            response.status_code = 404
+
+        return response
+
+
+class accountEvent(Resource):
     def get(self, user_id):
         user = next((u for u in users if u["id"] == user_id), None)
         if user:
@@ -40,8 +49,8 @@ class User(Resource):
             response.status_code = 200
         return response
 
-api.add_resource(UserList, '/users')
-api.add_resource(User, '/users/<int:user_id>')
+api.add_resource(accountEvent, '/event')
+api.add_resource(accountBalance, '/balance')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
