bcd108576b65aa1f77562c81281ba79f1a250ff6
Cainan Rodrigues <cainan.web@gmail.com>
2025-04-02 22:50:18 -0300

    Creation of the reset and accountEvent class with accountDeposit method

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request
+import json
+from collections import OrderedDict
+from flask import Flask, jsonify, request, Response
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -9,7 +11,7 @@ accounts = []
 class accountBalance(Resource):
     def get(self):
         try:
-            account_id = int(request.args.get('account_id'))
+            account_id = request.args.get('account_id')
         except Exception:
             response = jsonify({"message": "Invalid or missing account_id"})
             response.status_code = 400
@@ -18,7 +20,7 @@ class accountBalance(Resource):
         account = next((acc for acc in accounts if acc["id"] == account_id), None)
 
         if account:
-            response = jsonify(account["value"])
+            response = jsonify(account["balance"])
             response.status_code = 200
         else:
             response = jsonify(0)
@@ -28,29 +30,60 @@ class accountBalance(Resource):
 
 
 class accountEvent(Resource):
-    def get(self, user_id):
-        user = next((u for u in users if u["id"] == user_id), None)
-        if user:
-            return jsonify(user)
-        response = jsonify({"message": "User not found"})
-        response.status_code = 404
-        return response
+    def post(self):
+        event = request.get_json()
 
-    def delete(self, user_id):
-        global users
-
-        if not any(user["id"] == user_id for user in users):
-            response = jsonify({"message": "User not found"})
-            response.status_code = 404
+        if event["type"] == "deposit":
+            response = self.accountDeposit(event)
+       #elif event["type"] == "withdraw":
+           #response = self.accountWithdraw(event)
+       #elif event["type"] == "transfer":
+           #response = self.accountTransfer(event)
         else:
-            users[:] = [u for u in users if u["id"] != user_id]
+            response = jsonify({"message": "Invalid event type"})
+            response.status_code = 404
 
-            response = jsonify({"message": "User deleted"})
-            response.status_code = 200
         return response
+
+    def accountDeposit(self, event):
+        global accounts
+
+        for i, acc in enumerate(accounts):
+            if acc["id"] == event["destination"]:
+                accounts[i]["balance"] += event["amount"]
+                break
+        else:
+            accounts.append({"id": event["destination"], "balance": event["amount"]})
+
+        updated_account = next(acc for acc in accounts if acc["id"] == event["destination"])
+        ordered_response = OrderedDict([
+            ("destination", OrderedDict([
+                ("id", updated_account["id"]),
+                ("balance", updated_account["balance"])
+            ]))
+        ])
+
+        json_response = json.dumps(ordered_response)
+        return Response(json_response, status=201, mimetype='application/json')
+
+    def accountWithdraw(self, event):
+        return
+
+    def accountTransfer(self, event):
+        return
+
+
+class resetAccounts(Resource):
+    def post(self):
+        global accounts
+
+        accounts[:] = []
+
+        return
 
 api.add_resource(accountEvent, '/event')
 api.add_resource(accountBalance, '/balance')
+api.add_resource(resetAccounts, '/reset')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
